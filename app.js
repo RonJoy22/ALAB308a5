2cbf80d0092ad4fd6f64bcdecece100fb79a5cae
ronald.joyner-brown <r.brownjoyner@gmail.com>
2024-09-17 11:56:02 -0400

    lab done

---
+++ app.js
@@ -0,0 +1,12 @@
+import {maxCapacity , overGrown} from './growth.js';
+const PI = 3.1415;
+const radius = 5;
+const area = PI * radius * radius
+console.log(area)
+
+
+const plantSpace = .8;
+const plant = 100;
+const newArea = area * 5
+console.log(newArea)
+
